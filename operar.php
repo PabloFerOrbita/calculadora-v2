2fad6a8b0e6b08b594cb5abb3a1f8d65d176abe4
PabloFerOrbita <pablofernandezorbitadigital@gmail.com>
2024-04-12 14:34:07 +0200

    borradas las operaciones de operar.php

--- operar.php
+++ operar.php
@@ -6,21 +6,7 @@ require_once 'Calculadora.php';
 $calculadora = new Calculadora();
 
 
-    switch ($operador) {
-        case '-':
-            $resultado = $calculadora->restar($resultado, $numero);
-            break;
-        case '+':
-            $resultado =   $calculadora->sumar($resultado, $numero);
-            break;
-        case '*':
-            $resultado = $calculadora->multiplicar($resultado, $numero);
-            break;
-        case '/':
-            $resultado = $calculadora->dividir($resultado, $numero);
-            break;
-        default:
-            $resultado = intval($numero);
+   
     }
 
     echo json_encode(array('numero' => $numero, 'operador' => $operador, 'resultado' => $resultado));
